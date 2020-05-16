8150534e72bef076216fa74154fd29a96eb9ed3e
Aaron <roycet000@gmail.com>
2020-05-16 16:39:16 -0700

    Add files via upload

---
+++ AgeInitial.java
@@ -0,0 +1,17 @@
+package lab04;
+
+public class AgeInitial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int age = 39;
+		char middleInitial = 'T';
+		
+		System.out.println("My name is Ted" + " " + middleInitial + " " + "Johnson\n" + ":Age: " + age);
+		
+		
+		
+	}
+
+}
---
+++ StudentScores.java
@@ -0,0 +1,19 @@
+package lab04;
+
+public class StudentScores {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int testScore01 = 77;
+		int testScore02 = 78;
+		int testScore03 = 87;
+		int testScore04 = 91;
+		
+		int average = (testScore01 + testScore02 + testScore03 + testScore04)/4;
+		
+		System.out.println("The test average is " + average); 
+		
+	}
+
+}
